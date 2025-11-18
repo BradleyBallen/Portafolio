aefb79e11f7d1527b371e8655c5da4dc9c335442
Bradley <bradleyballen162004@gmail.com>
2025-11-17 23:21:26 -0500

    was corrected errors

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useMemo } from "react";
+import { useState, useMemo, JSX } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   SiNextdotjs,
